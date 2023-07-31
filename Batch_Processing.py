067319b9f84fbca5584b426a209498dad8fdf035
vanessadao31 <vanessadao31@yahoo.co.uk>
2023-07-31 16:18:14 +0100

    napari save layers

--- Batch_Processing.py
+++ Batch_Processing.py
@@ -18,6 +18,8 @@ from Draft import redirect_segmentation, load_file, binary_threshold, local_maxi
 directory = Path.cwd() / "Data"
 channels = 2
 channel_names = ["pores", "nucleus"]
+columns = ['File', 'Skimage Blobs', 'Clesperanto Blobs']
+total_rows = []
 
 for folder_path in directory.glob("A375M2_NUP96*"):
     
@@ -41,15 +43,27 @@ for folder_path in directory.glob("A375M2_NUP96*"):
 
         final_pores = np.reshape(pos_points, (-1, 3))
         final_pores2 = local_maxima(segmented_pores, binary)
+        flat_3D = segmented_pores.ravel()
         
         skimage_name = f"{folder_path}_skimage.csv"
         clesperanto_name = f"{folder_path}_clesperanto.csv"
+        data_name = f"{folder_path}_napari.csv"
+        
+        row = [folder_path.stem, final_pores.shape[0], final_pores2.shape[0]]
+        total_rows = np.append(total_rows, row, axis=0)
         
     np.savetxt(skimage_name, final_pores, delimiter=',')
     np.savetxt(clesperanto_name, final_pores2, delimiter=',')
-        
+    np.savetxt(data_name, flat_3D, delimiter=',')
+
     for csv_path in directory.glob("*.csv"):
         new_path = folder_path / csv_path.name
         csv_path.replace(new_path)
      
-            
+total_rows = np.reshape(total_rows, (-1, len(columns)))
+
+# saving results
+with open("summary_file.csv", mode='w') as summary_file:
+    summary_writer = csv.writer(summary_file, delimiter=',')
+    summary_writer.writerow(columns)        
+    summary_writer.writerows(total_rows)
